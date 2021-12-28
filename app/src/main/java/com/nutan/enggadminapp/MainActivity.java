cc25fbc5f5db461d8c0d7b6d9179dee01c45b85d
Shreyash <shreyash.pawar26@gmail.com>
2021-12-28 11:10:24 +0530

    Login Activity done

--- app/src/main/java/com/nutan/enggadminapp/MainActivity.java
+++ app/src/main/java/com/nutan/enggadminapp/MainActivity.java
@@ -2,13 +2,47 @@ package com.nutan.enggadminapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class MainActivity extends AppCompatActivity {
+
+     TextInputLayout mobNoLayout,pwLayout;
+     String mobNo,password;
+     Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        DocumentReference docRef = db.collection("Admin").document("doc1");
+
+        
+
+
+        mobNoLayout = findViewById(R.id.mobNo);
+        pwLayout = findViewById(R.id.password);
+        login = findViewById(R.id.login_button);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                mobNo = mobNoLayout.getEditText().getText().toString();
+                password = pwLayout.getEditText().getText().toString();
+
+
+                startActivity(new Intent(MainActivity.this,EnterData.class));
+            }
+        });
     }
 }
