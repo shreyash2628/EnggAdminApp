72c0cfabe3cd9494b2b10802c27b139c388ae766
Shreyash <shreyash.pawar26@gmail.com>
2022-01-05 18:43:06 +0530

    Layout finally done

--- app/src/main/java/com/nutan/enggadminapp/EnterData.java
+++ app/src/main/java/com/nutan/enggadminapp/EnterData.java
@@ -2,31 +2,104 @@ package com.nutan.enggadminapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class EnterData extends AppCompatActivity {
- //   TextView colgNameTv,colgAddTv,feesTv,cetScoreTv,placementPercentageTv;
 
-    EditText colgNameEt,colgAddEt,feesEt,cetScoreEt,placementPercentageEt;
-    Button imgBtn,saveBtn;
+    String colgNameStr,colgAddStr,feesStr,cetScoreStr,placementPercentageStr,colgDescriptionStr;
+    EditText colgNameEt,colgAddEt,feesEt,cetScoreEt,placementPercentageEt,addBranch,colgDescription;
+    Button imgBtn,uploadDataBtn,AddBranchBtn,doneBtn;
+    String branch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_enter_data);
 
-
+        colgDescription = findViewById(R.id.ColgDescriptionEt);
+        addBranch = findViewById(R.id.addBranchesEt);
         colgNameEt =findViewById(R.id.colg_name_et);
         colgAddEt =findViewById(R.id.address_Et);
         feesEt =findViewById(R.id.feesEt);
         cetScoreEt =findViewById(R.id.cetScoreEt);
         placementPercentageEt =findViewById(R.id.placementPercentageEt);
 
-        imgBtn = findViewById(R.id.colgImgIdBtn);
-        saveBtn = findViewById(R.id.SaveBtnId);
+        imgBtn = findViewById(R.id.AddcolgImgId);
+        uploadDataBtn = findViewById(R.id.upload_Data_Btn);
+
+        AddBranchBtn = findViewById(R.id.AddBranchBtn);
+        doneBtn = findViewById(R.id.DoneBtnId);
+
+
+        branch=addBranch.getText().toString();
+
+            if(branch.isEmpty())
+            {
+                doneBtn.setVisibility(View.INVISIBLE);
+
+            }
+
+        List<String> branchList = new ArrayList<>();
+
+        AddBranchBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                branch=addBranch.getText().toString();
+
+
+
+                branchList.add(branch);
+
+                doneBtn.setVisibility(View.VISIBLE);
+                Toast.makeText(EnterData.this,branch + " Branch Added", Toast.LENGTH_SHORT).show();
+                   addBranch.setText(null);
+            }
+        });
+
+        doneBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //upload branch list to firebase
+
+
+                Toast.makeText(EnterData.this, "Added successfully", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        Map<String,String> map = new HashMap<String,String>();
+        uploadDataBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                colgNameStr = colgNameEt.getText().toString();
+                colgAddStr = colgAddEt.getText().toString();
+                feesStr = feesEt.getText().toString();
+                cetScoreStr = cetScoreEt.getText().toString();
+                placementPercentageStr = placementPercentageEt.getText().toString();
+                colgDescriptionStr = colgDescription.getText().toString();
+
+                map.put("collegeName",colgNameStr);
+                map.put("Address",colgAddStr);
+                map.put("Fees",feesStr);
+                map.put("PlacementPercentage",placementPercentageStr);
+                map.put("CollegeDescription",colgDescriptionStr);
+                map.put("CetScore",cetScoreStr);
+
+                Toast.makeText(EnterData.this, "Uploaded Successfully", Toast.LENGTH_SHORT).show();
+            }
+        });
 
 
     }
--- app/src/main/java/com/nutan/enggadminapp/MainActivity.java
+++ app/src/main/java/com/nutan/enggadminapp/MainActivity.java
@@ -1,21 +1,29 @@
 package com.nutan.enggadminapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
      TextInputLayout mobNoLayout,pwLayout;
      String mobNo,password;
      Button login;
+     TextView invalidTv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,22 +35,65 @@ public class MainActivity extends AppCompatActivity {
         DocumentReference docRef = db.collection("Admin").document("doc1");
 
 
-
-
+        invalidTv = findViewById(R.id.invalidCredentialsTv);
         mobNoLayout = findViewById(R.id.mobNo);
         pwLayout = findViewById(R.id.password);
         login = findViewById(R.id.login_button);
+
+
 
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                mobNo = mobNoLayout.getEditText().getText().toString();
+                //   mobNo = mobNoLayout.getEditText().getText().toString();
                 password = pwLayout.getEditText().getText().toString();
+                startActivity(new Intent(MainActivity.this, EnterData.class));
 
 
-                startActivity(new Intent(MainActivity.this,EnterData.class));
+                mobNo = Objects.requireNonNull(mobNoLayout.getEditText().getText()).toString().trim();
+
+
+                if (!(mobNo.isEmpty() || mobNo.length() < 10)) {
+                    db.collection("Admin").whereEqualTo("mobile", mobNo).limit(1)
+                            .get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<QuerySnapshot> task) {
+
+                            if (task.isSuccessful()) {
+
+                                db.collection("Admin").whereEqualTo("password", password)
+                                        .get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                        if (task.isSuccessful()) {
+
+                                            //change activity
+
+                                        } else {
+                                            invalidTv.setVisibility(View.VISIBLE);
+                                        }
+
+                                    }
+                                });
+
+                            }
+
+
+                        }
+
+
+                        //onclicl method end
+                    });
+
+
+                }
+
             }
         });
-    }
-}
+
+
+    }}
+
+
+
