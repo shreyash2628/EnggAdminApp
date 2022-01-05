9f332ff2130c5809a11ea5756519cbaaeb176c8d
Shreyash <shreyash.pawar26@gmail.com>
2022-01-05 12:07:55 +0530

    Layout almost done

--- app/src/main/java/com/nutan/enggadminapp/EnterData.java
+++ app/src/main/java/com/nutan/enggadminapp/EnterData.java
@@ -3,12 +3,15 @@ package com.nutan.enggadminapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class EnterData extends AppCompatActivity {
  //   TextView colgNameTv,colgAddTv,feesTv,cetScoreTv,placementPercentageTv;
+
     EditText colgNameEt,colgAddEt,feesEt,cetScoreEt,placementPercentageEt;
+    Button imgBtn,saveBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +24,9 @@ public class EnterData extends AppCompatActivity {
         feesEt =findViewById(R.id.feesEt);
         cetScoreEt =findViewById(R.id.cetScoreEt);
         placementPercentageEt =findViewById(R.id.placementPercentageEt);
+
+        imgBtn = findViewById(R.id.colgImgIdBtn);
+        saveBtn = findViewById(R.id.SaveBtnId);
 
 
     }
